d710a0fc2230e0ce6e10832296dbb18b09ba7888
atcupps <91998663+atcupps@users.noreply.github.com>
2023-05-04 20:03:55 -0400

    Final lab 6 modifications

--- BO_AC_6/lab6final.py
+++ BO_AC_6/lab6final.py
@@ -14,13 +14,14 @@ def line_unbound(x, a, b):
     return a*x + b
 popt_ub, pcov_ub = curve_fit(line_unbound, distances, velocities)
 sigma_fit_ub = np.sqrt(np.diag(pcov_ub))
+sigma_fit_ub = np.rint(sigma_fit_ub)
 
 # Finding line of best fit for a zero intercept
 def line_bound(x, a):
     return a*x
 popt_b, pcov_b = curve_fit(line_bound, distances, velocities)
 sigma_fit_b = np.sqrt(np.diag(pcov_b))
-
+sigma_fit_b = np.rint(sigma_fit_b)
 
 # Plotting
 plt.figure()
@@ -29,8 +30,8 @@ plt.figure()
 distances_modified = np.append(distances, 0)
 
 # Drawing line of best fit
-plt.plot(distances_modified, line_unbound(distances_modified, *popt_ub), color="red", label="Non-zero intercept:\n" + r'$H_0 = $' + str(popt_ub[0])[0:4] + r'$\pm$' + str(sigma_fit_ub[0])[0:3] + "\n" + r'$v(0) = $' + str(popt_ub[1])[0:4] + r'$\pm$' + str(sigma_fit_ub[1])[0:5])
-plt.plot(distances_modified, line_bound(distances_modified, *popt_b), color="blue", label="Zero intercept:\n" + r'$H_0 = $' + str(popt_b[0])[0:4] + r'$\pm$' + str(sigma_fit_b[0])[0:3])
+plt.plot(distances_modified, line_unbound(distances_modified, *popt_ub), color="red", label="Non-zero intercept:\n" + r'$H_0 = $' + str(popt_ub[0])[0:2] + r'$\pm$' + str(sigma_fit_ub[0])[0:1] + "\n" + r'$v(0) = $' + str(popt_ub[1])[0:2] + r'$\pm$' + str(sigma_fit_ub[1])[0:3])
+plt.plot(distances_modified, line_bound(distances_modified, *popt_b), color="blue", label="Zero intercept:\n" + r'$H_0 = $' + str(popt_b[0])[0:2] + r'$\pm$' + str(sigma_fit_b[0])[0:1])
 
 # Plotting data with error bars
 plt.errorbar(x=distances, y=velocities, xerr=sigma_dist, yerr=sigma_v, ls="none", capsize=2, color="black", elinewidth=1)
